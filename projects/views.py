c7b4c9fa8f07b8a8f2845206e858d4d8dd5e7256
Vladimir_Kirichenko <frozenfish@internet.ru>
2022-07-27 14:31:19 +0300

    Прописаны urls на главную страницу и страницу проекта. Созданы соответствуующие templates

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Project
+
+
+def index(request):
+    """главная страница - выводит все проекты"""
+    projects = Project.objects.all()
+
+    template = 'projects/index.html'
+
+    context = {'projects': projects}
+
+    return render(request, template, context)
+
+
+def project(request, slug):
+    """выводит информацию о конкретном проекте"""
+    project = get_object_or_404(Project, slug=slug)
+    performers = project.performer.all()
+
+    template = 'projects/project.html'
+
+    context = {
+        'project': project,
+        'performers': performers
+    }
+
+    return render(request, template, context)
+
